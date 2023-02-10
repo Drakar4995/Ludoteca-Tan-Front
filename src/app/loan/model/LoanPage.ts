0b2ec3bcc6690ad7f161d53803d6ddb3db0786a4
Nikolai <the-inic@hotmail.com>
2023-02-10 10:37:24 +0100

    Creada LoanPage
    Para la paginacion del Loan

---
+++ src/app/loan/model/LoanPage.ts
@@ -0,0 +1,8 @@
+import { Loan }from "./Loan";
+import {Pageable} from "src/app/core/model/page/Pageable";
+
+export class LoanPage{
+    content: Loan[];
+    pageable: Pageable;
+    totalElements:number;
+}
